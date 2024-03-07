42e1eb47906533f54c9bf8a484f703e0d49302dc
jurko <jurko.bossetroll@gmail.com>
2024-03-07 18:31:02 +0100

    Change TestLeafNode tests

--- src/test_htlmnode.py
+++ src/test_htlmnode.py
@@ -37,11 +37,11 @@ class TestHTMLNode(unittest.TestCase):
 
 
 class TestLeafNode(unittest.TestCase):
-    def test_to_html_without_props(self):
+    def test_leaf_node_to_html_without_props(self):
         node = LeafNode("p", "This is a paragraph of text.")
         self.assertEqual("<p>This is a paragraph of text.</p>", node.to_html())
 
-    def test_to_html_with_props(self):
+    def test_leaf_node_to_html_with_props(self):
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual("<a href=\"https://www.google.com\">Click me!</a>", node.to_html())
 
