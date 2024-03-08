1e7f9569d856464dbb2adeb464a7f1a868fc7798
jurko <jurko.bossetroll@gmail.com>
2024-03-08 18:20:07 +0100

    Add text_node_to_html_node function

---
+++ src/text_to_html.py
@@ -0,0 +1,21 @@
+from htmlnode import LeafNode
+from textnode import TextNode, TextType
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    node_type = text_node.text_type
+    text = text_node.text
+    if node_type is not TextType:
+        raise TypeError("TextNode type expected to be TextType member")
+    if node_type == TextType.TEXT:
+        return LeafNode(None, text)
+    if node_type == TextType.BOLD:
+        return LeafNode("b", text)
+    if node_type == TextType.ITALIC:
+        return LeafNode("i", text)
+    if node_type == TextType.CODE:
+        return LeafNode("code", text)
+    if node_type == TextType.LINK:
+        return LeafNode("a", text, {"href": text_node.url})
+    if node_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
