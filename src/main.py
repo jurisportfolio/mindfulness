12dd6a276fbf1bb28e61a6af0af330b847473fac
jurko <jurko.bossetroll@gmail.com>
2024-03-09 22:03:56 +0100

    Change arguments for new_node object

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode, TextType
 
 
 def main():
-    new_node = TextNode("My new node", "bold", "https//new-node.com")
+    new_node = TextNode("My new node", TextType.BOLD, "https//new-node.com")
     print(new_node.__repr__())
 
 
